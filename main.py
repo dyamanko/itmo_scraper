b73ca79d5e368c3b19d998fe90f84a72c6d0de97
yamanko.d <yamankodmitry@gmail.com>
2022-10-17 03:02:13 +0300

    UPD: work on project structure

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('hi')
+def parse(url):
+    print(url)
+
+
+# поисковый запрос на сайте habr.com. параметры: number - номер страницы, search - текст поискового запроса
+list_url = "https://habr.com/ru/search/page{number}/?q={search}&target_type=posts&order=relevance"
+
+# текст поискового запроса
+search_text = "javascript"
+
+# кол-во страниц
+max_page_number = 20
+
+# номер стартовой страницы
+page_number = 1
+
+
+while page_number <= max_page_number:
+    parse(list_url.format(number=page_number, search=search_text))
+    page_number += 1
